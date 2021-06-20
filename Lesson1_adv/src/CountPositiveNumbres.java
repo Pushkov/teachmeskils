7906d7a34adaad51d1e772a42a5dc0db5b9d4e44
Pushkov <nicsx@mail.ru>
2021-06-20 16:50:09 +0300

    First lesson additional task

---
+++ Lesson1_adv/src/CountPositiveNumbres.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CountPositiveNumbres {
+
+    public static void main(String[] args) {
+        int countPositive = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter some numbers: ");
+        String list = scan.nextLine();
+        scan = new Scanner(list);
+        while (scan.hasNext()) {
+            int num = scan.nextInt();
+            if(num >= 0){
+                countPositive ++;
+            }
+        }
+        scan.close();
+        System.out.println("Count of positive numbers: " + countPositive);
+    }
+}
