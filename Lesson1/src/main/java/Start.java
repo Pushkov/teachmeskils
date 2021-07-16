fb5ec7d01bea4b9dcae4fd5e4918430f127e268f
Pushkov <nicsx@mail.ru>
2021-07-16 09:14:52 +0300

    add in all projects pom.xml

--- Lesson1/src/main/java/Start.java
+++ Lesson1/src/main/java/Start.java
@@ -1,3 +1,4 @@
+
 public class Start {
     public static void main(String[] args) {
         System.out.println("Hello, little world!!!");
---
+++ Lesson1_adv/src/main/java/CountPositiveNumbers.java
@@ -0,0 +1,19 @@
+//import java.util.Scanner;
+public class CountPositiveNumbers {
+    public static void main(String[] args) {
+//        int countPositive = 0;
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter some numbers: ");
+//        String list = scan.nextLine();
+//        scan = new Scanner(list);
+//        while (scan.hasNext()) {
+//            int num = scan.nextInt();
+//            if(num >= 0){
+//                countPositive ++;
+//            }
+//        }
+//        scan.close();
+//        System.out.println("Count of positive numbers: " + countPositive);
+        System.out.println("111");
+    }
+}
