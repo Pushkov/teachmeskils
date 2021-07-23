069229c0301ef1ff68bd056dbd6925f6e1476765
Pushkov <nicsx@mail.ru>
2021-07-23 12:47:06 +0300

    Lesson9_wear Корректировка метода получения различных случайных Items отличных от конкретного Item.

--- Lesson9_wear/src/main/java/com/home/util/HomeWork9WearUtils.java
+++ Lesson9_wear/src/main/java/com/home/util/HomeWork9WearUtils.java
@@ -6,18 +6,26 @@ public class HomeWork9WearUtils {
     private static final SecureRandom RANDOM = new SecureRandom();
 
     public static <T extends Enum> T getRandomEnumItem(Class<T> clazz) {
-        T[] enumConstatnts = clazz.getEnumConstants();
-        int randomIndex = RANDOM.nextInt(enumConstatnts.length);
-        return enumConstatnts[randomIndex];
+        if (clazz != null) {
+            T[] enumConstatnts = clazz.getEnumConstants();
+            int randomIndex = RANDOM.nextInt(enumConstatnts.length);
+            return enumConstatnts[randomIndex];
+        } else {
+            return null;
+        }
     }
 
     public static <T extends Enum> T getOtherRandomItem(T item) {
-        T resultItem;
-        do {
-            resultItem = (T) getRandomEnumItem(item.getClass());
-            System.out.println("> " + resultItem);
-        } while (resultItem == item);
-        return resultItem;
+        if (item != null) {
+            T resultItem;
+            do {
+                resultItem = (T) getRandomEnumItem(item.getClass());
+//                System.out.println("> " + resultItem);
+            } while (resultItem == item);
+            return resultItem;
+        } else {
+            return null;
+        }
     }
 
     public static boolean getRandomBoolean() {
