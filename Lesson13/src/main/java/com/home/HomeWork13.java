541e9a597406a013fe36d82578d180e3761dca8f
nicomed <nicsx@mail.ru>
2021-08-01 23:38:24 +0300

    Lesson13 ДР_0.2

--- Lesson13/src/main/java/com/home/HomeWork13.java
+++ Lesson13/src/main/java/com/home/HomeWork13.java
@@ -1,16 +1,21 @@
 package com.home;
 
 import static com.home.util.Constants.SMALL_TEXT;
+import static com.home.util.Constants.TEXT;
 import static org.apache.commons.lang3.StringUtils.reverse;
 
 public class HomeWork13 {
     public static void main(String[] args) {
-        String text = SMALL_TEXT;
         System.out.println(SMALL_TEXT);
-        System.out.println("1) " + homeWork1(text));
-        System.out.println("2) " + homeWork2(text));
+        System.out.println("1) " + homeWork1(SMALL_TEXT));
+        System.out.println("2) " + homeWork2(SMALL_TEXT));
         System.out.println("3)");
-        homeWork3(text);
+        homeWork3(SMALL_TEXT);
+        System.out.println("4)");
+        homeWork4(TEXT);
+        System.out.println("4.1)");
+        homeWork4(SMALL_TEXT);
+
 
     }
 
@@ -33,4 +38,29 @@ public class HomeWork13 {
             }
         }
     }
+
+    private static void homeWork4(String text) {
+        String editedText = text;
+        String[] words = editedText.split("\\.");
+        for (String word : words) {
+//            if (word.equals(reverse(word)) && word.length() > 1) {
+            if (word.split(" ").length > 2 && word.split(" ").length < 6 || isPalindromeExists(word)) {
+                System.out.println(word);
+            }
+        }
+//        System.out.println(words.length);
+    }
+
+    private static boolean isPalindromeExists(String text) {
+        boolean isPalindromeExists = false;
+        String editedText = text.replaceAll("[(),.:;!?{}-]", "");
+        String[] words = editedText.split(" ");
+        for (String word : words) {
+            if (word.equals(reverse(word)) && word.length() > 1) {
+                isPalindromeExists = true;
+            }
+        }
+        return isPalindromeExists;
+    }
+
 }
