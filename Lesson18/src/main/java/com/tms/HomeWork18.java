94e8be42b746529926d5f5c1ba560680f34ae9e8
Pushkov <nicsx@mail.ru>
2021-08-23 12:12:53 +0300

    Lesson18 Второй вариант домашнего задания

--- Lesson18/src/main/java/com/tms/HomeWork18.java
+++ Lesson18/src/main/java/com/tms/HomeWork18.java
@@ -1,6 +1,7 @@
 package com.tms;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.tms.model.CashRegister;
 import com.tms.model.Product;
 import com.tms.model.User;
 import com.tms.produce.Factory;
@@ -12,23 +13,57 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
 public class HomeWork18 {
-    public static final int CASH_REGISTER = 5;
-    public static final int USERS_COUNT = 12;
-    public static final int PRODUCT_LIMIT = 5;
+    public static final int CASH_REGISTER = 3;
+    public static final int USERS_COUNT = 7;
+    public static final int PRODUCT_LIMIT = 2;
     public static Random random = new Random();
 
+    private static Factory factory;
+    private static List<User> users;
+
     public static void main(String[] args) {
-        Factory factory = Factory.getInstance();
-        List<User> users = factory.create(User.class, USERS_COUNT);
+        factory = Factory.getInstance();
+        users = factory.create(User.class, USERS_COUNT);
         users.forEach(u -> u.setProducts(factory.create(Product.class, getProductCount())));
         users.forEach(System.out::println);
+//        threadPoolMethod();
+        lockMethod();
+    }
 
+    private static void threadPoolMethod() {
+        System.out.println("Разбор единой очереди с помощью пул-а потоков.");
         final ThreadFactory threadFactory = new ThreadFactoryBuilder()
-                .setNameFormat(" №-%d")
+                .setNameFormat(" Касса №-%d")
                 .build();
         final ExecutorService service = Executors.newFixedThreadPool(CASH_REGISTER, threadFactory);
         users.forEach(service::submit);
         service.shutdown();
+    }
+
+    private static void lockMethod() {
+        System.out.println("Разбиение покупателей на очереди для каждой кассы и обслуживание  каждого пользователся в кассе с использованием блокировки");
+        List<CashRegister> cashRegisters = factory.create(CashRegister.class, CASH_REGISTER);
+        List<CashRegister> usersQueues = splitUsersByCashRegistry(users, cashRegisters);
+        usersQueues.forEach(x -> System.out.println(x + "\n"));
+        usersQueues.forEach(cashRegister -> cashRegister.getQueue().forEach(user -> cashRegistryService(cashRegister, user)));
+    }
+
+    private static void cashRegistryService(CashRegister cass, User user) {
+        user.setCass(cass);
+        Thread thread = new Thread(user, cass.getName());
+        thread.start();
+    }
+
+    private static List<CashRegister> splitUsersByCashRegistry(List<User> users, List<CashRegister> cashRegisters) {
+        int index = 0;
+        for (User u : users) {
+            if (index >= cashRegisters.size()) {
+                index = 0;
+            }
+            cashRegisters.get(index).getQueue().add(u);
+            index++;
+        }
+        return cashRegisters;
     }
 
     private static int getProductCount() {
