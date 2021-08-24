037e1075fa82c402d1fd1b91783091c0c2dc7a6e
Pushkov <nicsx@mail.ru>
2021-08-24 16:08:21 +0300

    main TelegramBot v2.0.2

--- MyProjects/TelegramSpring/src/main/java/nicomed/tms/telegramspring/service/DataService.java
+++ MyProjects/TelegramSpring/src/main/java/nicomed/tms/telegramspring/service/DataService.java
@@ -9,32 +9,20 @@ import java.util.List;
 public interface DataService {
 
     City findCityByName(String name);
-
     List<City> findAllCities();
-
     boolean addCity(String cityName);
-
     boolean editCity(String cityName);
-
     boolean removeCity(String cityName);
 
     Place findPlaceById(Long id);
-
     Place findPlaceByName(String name);
-
     List<Place> findAllPlaces();
-
     List<Place> findAllPlacesByCity(City city);
-
     List<Place> findAllPlacesByCityName(String cityName);
-
     boolean addPlace(String placeName, Grade drade);
-
     boolean editPlace(String placeName);
-
     boolean removePlace(String placeName);
 
     boolean addPlaceToCity(Place place, City city);
-
     boolean removePlaceFromCity(Place place, City city);
 }
